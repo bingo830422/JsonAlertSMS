b6bf6c6bf953bb5e43bcc774686a0c89228716b8
Jayden Yang <jaydenyang@CCs-Mac.local>
2022-09-10 20:56:15 +0800

    Spring boot initialize

---
+++ src/test/java/com/yang/jsonalertsms/JsonalertsmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yang.jsonalertsms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JsonalertsmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
